9c6b2104664ba3fe75e938fe00e21c38201e5cf2
Aditya Chaudhary <aditya946c@gmail.com>
2021-06-04 20:02:06 +0530

    first commit

---
+++ app/src/main/java/com/example/birthday/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.birthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button2.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage = findViewById<ImageView>(R.id.imageView)
+        val drawableId= when(diceRoll){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableId)
+        diceImage.contentDescription.toString()
+    }
+}
+
+class Dice(private val sides: Int) {
+    fun roll(): Int {
+        val num = 1..sides
+        return num.random()
+    }
+}
